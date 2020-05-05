19d4f206771b2021d0e5ff802008a259f0a5cfa6
Proheeler <skemobob@gmail.com>
2020-05-05 15:28:02 +0000

    Profiling + warp size and channels args added

--- cuda1/include/cuda_fd_funcs.h
+++ cuda1/include/cuda_fd_funcs.h
@@ -3,7 +3,7 @@
 #include <cstdint>
 // System includes
 
-void h_convertRGBtoGrayScale(uint8_t* src,uint8_t* dst,int && width, int&& height);
+void h_convertRGBtoGrayScale(uint8_t* src,uint8_t* dst,int TileSize,int && width, int&& height,int && channels);
 
 
 #endif // CUDA_FD_FUNCS_H
--- cuda1/src/main.cpp
+++ cuda1/src/main.cpp
@@ -5,16 +5,39 @@
 #include <cuda.h>
 #include <iostream>
 #include <QImage>
+#include <chrono>
+class Profiler
+{
+public:
+    Profiler(){
+        start = std::chrono::system_clock::now();
+    }
+    ~Profiler(){
+        std::chrono::time_point<std::chrono::system_clock> end = std::chrono::system_clock::now();
+        std::chrono::duration<float> difference = end - start;
+        std::cout<<difference.count()*1000<<std::endl;
+    }
+private:
+    std::chrono::time_point<std::chrono::system_clock> start;
+};
+
+
 auto main() -> int
 {
-QImage img("/home/dev/development/cuda_exploration/build/05.jpeg");
-img.convertTo(QImage::Format::Format_RGB888);
-img.save("/home/dev/development/cuda_exploration/build/test1.jpg");
-uint8_t* dst = new uint8_t[img.width()*img.height()];
-h_convertRGBtoGrayScale(img.bits(),dst,img.width(),img.height());
-
-
-QImage frame = QImage(dst, img.width(), img.height(), img.width(), QImage::Format::Format_Grayscale8);
-frame.save("/home/dev/development/cuda_exploration/build/test.jpg");
-delete[] dst;
+    QImage img("/home/dev/development/cuda_exploration/build/SeaSunset.jpg");
+    {
+        Profiler p;
+        img.convertTo(QImage::Format::Format_RGB888);
+    }
+    img.save("/home/dev/development/cuda_exploration/build/test1.jpg");
+    std::cout<<img.depth()<<std::endl;
+    uint8_t* dst = new uint8_t[img.width()*img.height()];
+    h_convertRGBtoGrayScale(img.bits(),dst,16,img.width(),img.height(),img.depth()/8);
+    {
+        Profiler p;
+        h_convertRGBtoGrayScale(img.bits(),dst,16,img.width(),img.height(),img.depth()/8);
+    }
+    QImage frame = QImage(dst, img.width(), img.height(), img.width(), QImage::Format::Format_Grayscale8);
+    frame.save("/home/dev/development/cuda_exploration/build/test.jpg");
+    delete[] dst;
 }
